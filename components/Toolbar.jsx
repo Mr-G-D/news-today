fb62e7efd574faba7d68bde1a4fee05cf0b9f5b9
Mr-G-D <daredinesh00@gmail.com>
2023-04-03 12:44:29 +0530

    Complete

--- components/Toolbar.jsx
+++ components/Toolbar.jsx
@@ -1,10 +1,18 @@
 import { useRouter } from "next/router";
 import styles from "@/styles/Toolbar.module.css";
+import Head from "next/head";
 
 const Toolbar = () => {
   const router = useRouter();
   return (
     <div className={styles.main}>
+      <Head>
+        <title>News Today</title>
+        <link
+          rel="icon"
+          href="https://cdn.onlinewebfonts.com/svg/img_45428.png"
+        />
+      </Head>
       <div onClick={() => router.push("/")}>Home</div>
       <div onClick={() => router.push("/feed/1")}>Feed</div>
       <div onClick={() => router.push("/eom")}>EOM</div>
